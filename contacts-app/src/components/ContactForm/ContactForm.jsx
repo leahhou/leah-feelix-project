f6642d9e7755d5ef54cc28d663286268b1934a9c
leahhou <hsx_124@hotmail.com>
2020-08-20 14:32:12 +1000

    remove http link for image and use local file

--- contacts-app/src/components/ContactForm/ContactForm.jsx
+++ contacts-app/src/components/ContactForm/ContactForm.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "../Button/Button";
 import Input from "../Input/Input";
 import styles from "./ContactForm.module.css";
+import avatar1 from "./../../avatar-1.png";
 
 class ContactForm extends React.Component {
   constructor(props) {
@@ -83,5 +84,5 @@ class ContactForm extends React.Component {
 
 export default ContactForm;
 
-const defaultImage =
-  "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShaggyMullet&accessoriesType=Prescription02&hairColor=Brown&facialHairType=Blank&clotheType=GraphicShirt&clotheColor=White&graphicType=Resist&eyeType=Wink&eyebrowType=Default&mouthType=Smile&skinColor=Light";
+const defaultImage = avatar1;
+// "https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShaggyMullet&accessoriesType=Prescription02&hairColor=Brown&facialHairType=Blank&clotheType=GraphicShirt&clotheColor=White&graphicType=Resist&eyeType=Wink&eyebrowType=Default&mouthType=Smile&skinColor=Light";
