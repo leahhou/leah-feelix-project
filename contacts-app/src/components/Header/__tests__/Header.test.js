0caaf275f97923688bbbe62fd0fab0e9b7ccae11
leahhou <hsx_124@hotmail.com>
2020-08-31 14:01:41 +1000

    Add favicon and add more header test cases

--- contacts-app/src/components/Header/__tests__/Header.test.js
+++ contacts-app/src/components/Header/__tests__/Header.test.js
@@ -2,18 +2,48 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 //why do I need to import agin while setupTests.js already import?
 import "@testing-library/jest-dom/extend-expect";
-import App from "../../../layout/App";
+import Header from "../Header";
 
 describe("Header", () => {
-  test("renders Header component", () => {
-    render(<App />);
-
-    // screen.debug();
-
+  const mockHandleClick = jest.fn();
+  const pageTitle = "My Contacts";
+  const buttonText = "Add";
+  test("Header component should contains Page Title that passed as a prop", () => {
+    render(
+      <Header
+        type=""
+        text={buttonText}
+        header={pageTitle}
+        handleClick={mockHandleClick}
+      />
+    );
     // explicit assertion
     // recommended
     //use string for exact match, and regex for partial match
     expect(screen.getByText("My Contacts")).toBeInTheDocument();
-    // expect(screen.getByRole("heading")).toHaveTextContent("My Contacts");
+  });
+  test("Header component contains a button with text passed it as a prop", () => {
+    const mockHandleClick = jest.fn();
+    render(
+      <Header
+        type=""
+        text={buttonText}
+        header={pageTitle}
+        handleClick={mockHandleClick}
+      />
+    );
+    expect(screen.getByText("Add")).toBeInTheDocument();
+  });
+  test("Header component contains a button that is clickable", () => {
+    const mockHandleClick = jest.fn();
+    render(
+      <Header
+        type=""
+        text={buttonText}
+        header={pageTitle}
+        handleClick={mockHandleClick}
+      />
+    );
+    expect(screen.getByText("Add")).toBeInTheDocument();
   });
 });
